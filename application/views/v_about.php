3b7caebc437970f19b00ffdede0ff4fb2ef0d92b
imamfidiant <imamfidianto01@gmail.com>
2022-06-17 22:18:43 +0700

    initial commit

---
+++ application/views/v_about.php
@@ -0,0 +1,192 @@
+
+<!DOCTYPE html>
+<html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>About Us</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="Reva Nofiana" />
+	<link rel="shorcut icon" type="text/css" href="<?php echo base_url().'assets/images/favicon.png'?>">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/animate.css'?>">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/icomoon.css'?>">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.css'?>">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/flexslider.css'?>">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/style.css'?>">
+
+	<!-- Modernizr JS -->
+	<script src="<?php echo base_url().'theme/js/modernizr-2.6.2.min.js'?>"></script>
+
+	</head>
+	<body>
+
+
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="header-inner">
+				<h1><a href="<?php echo base_url().''?>">Rumah Sakit<span>.</span></a></h1>
+				<nav role="navigation">
+					<ul>
+						<li><a href="<?php echo base_url().''?>">Home</a></li>
+						<li class="active"><a href="<?php echo base_url().'about'?>">About</a></li>
+						<li><a href="<?php echo base_url().'portfolio'?>">Portfolio</a></li>
+						<li><a href="<?php echo base_url().'artikel'?>">Blog</a></li>
+						<li><a href="<?php echo base_url().'gallery'?>">Gallery</a></li>
+						<li><a href="<?php echo base_url().'kontak'?>">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+
+	<aside id="fh5co-hero" clsas="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url(<?php echo base_url().'theme/images/person4.jpg'?>);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>Who We Are</h2>
+		   					<p class="fh5co-lead">RSIA Trisna Medika</p>
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		  	</ul>
+	  	</div>
+	</aside>
+
+	<div class="fh5co-about animate-box">
+		<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+			<h2>Tentang Kami</h2>
+			<p>RSIA Trisna Medika merupakan rumah sakit ibu dan anak tipe C milik Swasta. Rumah sakit yang terletak di wilayah Kabupaten Tulungagung, Jawa Timur ini memberikan pelayanan di bidang kesehatan khusus ibu dan anak yang didukung oleh layanan dokter spesialis serta ditunjang dengan fasiitas medis lainnya.		</div>
+		<div class="container">
+			<div class="col-md-6">
+				<figure>
+					<img src="<?php echo base_url().'theme/images/bb.jpg'?>" alt="Free HTML5 Template" class="img-responsive">
+				</figure>
+			</div>
+			<div class="col-md-6">
+				<h3>Visi</h3>
+				<ul>
+					<li>Dalam proses perkembangan selanjutnya pelaksanaan pelayanan kesehatan pada masyarakat ditekankan dalam pemenuhan pelayanan yang prima yaitu kemudahan dalam proses,,terjamin dalam kepastian hukum,cepat dilayani,,tepat sasaran,,murah biaya dan aman.				</ul>
+				<h3>Misi</h3>
+				<ul>
+					<li>Komitmen Memberikan Pelayanan Terbaik</li>
+					<li>Komitmen Memberikan Pelayanan Terbaik</li>
+					<li>Komitmen Memberikan Pelayanan Terbaik</li>
+					<li>Komitmen Memberikan Pelayanan Terbaik</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="fh5co-team animate-box">
+		<div class="container">
+
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+						<h2>The Team</h2>
+						<p>Kami memiliki tim yang solid. one team, one spirit, and one goal.</p>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/93.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Reva Nofiana</h3>
+						<h4>Dokter Gizi</h4>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/95.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Ahmad Haikal</h3>
+						<h4>Dokter Anak</h4>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/90.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Annisa Qothrunnada</h3>
+						<h4>Dokter Bedah</h4>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/91.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Arfian Pansa P.P</h3>
+						<h4>Dokter Gigi</h4>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/92.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Nurul Imaniyah</h3>
+						<h4>Dokter Umum</h4>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="<?php echo base_url().'theme/images/93.jpg'?>" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>RevaNof</h3>
+						<h4>Dokter bedah</h4>
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+		</div>
+	</div>
+
+
+	
+
+
+	<?php $this->load->view('v_footer');?>
+	</div>
+
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+	<!-- jQuery Easing -->
+	<script src="<?php echo base_url().'theme/js/jquery.easing.1.3.js'?>"></script>
+	<!-- Bootstrap -->
+	<script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+	<!-- Waypoints -->
+	<script src="<?php echo base_url().'theme/js/jquery.waypoints.min.js'?>"></script>
+	<!-- Flexslider -->
+	<script src="<?php echo base_url().'theme/js/jquery.flexslider-min.js'?>"></script>
+
+	<!-- MAIN JS -->
+	<script src="<?php echo base_url().'theme/js/main.js'?>"></script>
+
+	</body>
+</html>
---
+++ application/views/v_footer.php
@@ -0,0 +1,39 @@
+<footer id="fh5co-footer" role="contentinfo">
+
+		<div class="container">
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>RUMAH SAKIT</h3>
+				<img width="250px" src="<?php echo base_url().'theme/images/11.jpeg'?>" alt="" srcset="">
+			</div>
+			<div class="col-md-3 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Our Services</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'portfolio'?>">Spesialis Kebidanan & Kandungan</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">Spesialis Anak</a></li>
+				</ul>
+
+			</div>
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Information Contact</h3>
+				<ul class="float">
+						<li><i class="icon-map"></i> Jl. Pahlawan
+						Dusun Ketanon, Ketanon
+						Kec. Kedungwaru
+						Kabupaten Tulungagung, Jawa Timur 66229</li>
+						<li><i class="icon-phone"></i> 0856 081 36654</li>
+						<li><i class="icon-envelope"></i><a href="http://google.com/"> revanofiana@gmail.com</a></li>
+					</ul>
+			</div>
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Follow Us</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+
+
+		</div>
+	</footer>
